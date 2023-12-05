9cb5ccf516a9772336585f31175b99fe6fbc6477
Roni Tarvainen <roni.tarvainen@gmail.com>
2023-12-05 21:11:18 +0200

    created post system

--- app/models.py
+++ app/models.py
@@ -1,2 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Post(models.Model):
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    is_public = models.BooleanField()
--- app/views.py
+++ app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.template import loader
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from .models import Post
 
 
 @login_required
-def index(request):
-    return render(request, "app/index.html")
+@csrf_protect
+def home(request):
+    return render(request, "app/home.html")
+
+@login_required
+@csrf_protect
+def post(request):
+    is_public = False
+    if (hasattr(request.POST, "is_public")):
+        is_public = True
+    post = Post(publisher = request.user, content = request.POST["message"], is_public=is_public)
+    post.save()
+    return redirect("/")
